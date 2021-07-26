6cf56ae47fc98631c600fc2f35c3b4df1837c443
habib <habibdev96@gmail.com>
2021-07-26 11:14:20 -0600

    created about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Globals from "./abstracts/Globals";
 import Showcase from "./components/Showcase";
+import About from "./components/About";
 
 const App = () => {
   return (
     <>
       <Globals />
       <Showcase />
+      <About />
     </>
   );
 };
---
+++ src/components/About.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import aboutImageDark from "../images/image-about-dark.jpg";
+import aboutImageLight from "../images/image-about-light.jpg";
+import { headingStyles, textStyles } from "../abstracts/Mixins";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: -4px;
+  align-items: center;
+
+  .about-img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .about-info {
+    padding: 5rem;
+  }
+
+  .about-title {
+    ${headingStyles}
+    color: var(--veryDarkGray);
+    font-size: var(--xl);
+    margin-bottom: 3rem;
+  }
+
+  .about-desc {
+    ${textStyles}
+    color: var(--darkGray);
+    font-size: var(--xxs);
+  }
+`;
+
+const About = () => {
+  return (
+    <section>
+      <Container>
+        <img className="about-img" src={aboutImageDark} alt="about" />
+        <div className="about-info">
+          <h2 className="about-title">About our furniture</h2>
+          <p className="about-desc">
+            Our multifunctional collection blends design and function to suit
+            your individual taste. Make each room unique, or pick a cohesive
+            theme that best express your interests and what inspires you. Find
+            the furniture pieces you need, from traditional to contemporary
+            styles or anything in between. Product specialists are available to
+            help you create your dream space.
+          </p>
+        </div>
+        <img className="about-img" src={aboutImageLight} alt="about" />
+      </Container>
+    </section>
+  );
+};
+
+export default About;
